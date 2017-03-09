f726c79453ad628a4c5c28cc428ccf0bf4197452
Saketh Vallakatla <saketh21v@gmail.com>
2017-03-09 19:25:43 +0530

    Vending logic done. IO needs work

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# ToC_Assignement1
+
+ToC_Assignement1
+===
+
+## Question 1
+
+
+
+
+
+## Question 3
+
+**Problem statement:** *Vending machine implementation with Moore Machine*
+
+**Language**: *python* 
+
+**Description:**
+
++ The machine has 4 states namely, 
+  + `0¢`
+  + `5¢`
+  + `10¢`
+  + `15¢`
++ Input symbols are : *5, 10 *
++ Final State: `15¢`
++ Final Condition: Output string should contain at least one '1'.
--- toc_three/Vending_Machine.py
+++ toc_three/Vending_Machine.py
@@ -1,30 +1,29 @@
 from Moore_Machine import MooreMachine
 
-name = "Vendoor"
-states = ('a', 'b', 'c', 'd')
-inAlphabet = ('0', '1')
-outAlphabet = ('x1', 'x2', 'x3')
+name = "15¢Vendor"
+states = ('0', '5', '10', '15')
+inAlphabet = ('5', '0')
+outAlphabet = ('0', '1')
 
 inTransition = {
-    'a' : {'0' : 'b', '1' : 'c'},
-    'b' : {'0' : 'b', '1' : 'd'},
-    'c' : {'0' : 'd', '1' : 'c'},
-    'd' : {'0' : 'd', '1' : 'd'}
+    '0' : {'5' : '5', '10' : '10'},
+    '5' : {'5' : '10', '10' : '15'},
+    '10' : {'5' : '15', '10' : '15'},
+    '15' : {'5' : '15', '10' : '15'}
 }
 
 outTransition = {
-    'a' : 'x2',
-    'b' : 'x1',
-    'c' : 'x2',
-    'd' : 'x3'
+    '0' : '0',
+    '5' : '0',
+    '10' : '0',
+    '15' : '1'
 }
 
-initState = 'a'
+initState = '0'
 
 vm = MooreMachine(name, states, inAlphabet, outAlphabet, inTransition, outTransition, initState)
 
-vm.input('1')
-
-print("State : " + vm.getState())
-# vm.input('0')
-print("Output : "+vm.getOutput())
+vm.input('5')
+print(vm.getOutput())
+vm.input('10')
+print(vm.getOutput())
